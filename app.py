2ef6ea96e207d3d8fa80d75be7bc4ab93b1aca69
meteyildirim <meteyildirim2005@gmail.com>
2021-01-08 21:55:44 +0100

    palmistry deployment

---
+++ app.py
@@ -0,0 +1,15 @@
+
+#import keras
+#from keras.preprocessing import image
+
+#model = tf.keras.models.load_model('model_palm_life_line_2010.h5')
+
+import streamlit as st
+
+def main():
+
+	st.title("Iris EDA App with streamlit")
+	
+if __name__ == '__name__':
+	main()
+
