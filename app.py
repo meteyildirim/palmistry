c242337d8056ecbaeb9ad5f14d83189d13dd3254
meteyildirim <meteyildirim2005@gmail.com>
2021-01-09 00:31:38 +0100

    all again

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def main():
 	st.write("This is a palm reading web app to predict your future")
 
 	file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
+	
 	image_sz = (180,180) 
 	
 	img = keras.preprocessing.image.load_img( file, target_size=image_sz )
@@ -23,27 +24,20 @@ def main():
 	img_array = keras.preprocessing.image.img_to_array(img)
 	
 	img_array = np.expand_dims(img_array, 0)  # Create batch axis	
+	
 	predictions = model.predict(img_array)
 	
-	max_indx = 0
-	max_num = 0
-	predictions = predictions.squeeze()
+	max_indx = np.argmax(predictions)
 	
-	for indx in  range(len(predictions)):
-	  if indx == 0:
-		max_num=predictions[indx]
-		max_indx=indx
-	  if indx>0:
-		if predictions[indx]>predictions[indx-1]:
-		  max_num=predictions[indx]
-		  max_indx=indx
-	#print(max_indx,". no:",max_num)
+	
 	if max_indx == 0:
-	  st.write("A hand with three lines (life-brain-heart) joined")
+		st.write("A hand with three lines (life-brain-heart) joined")
+		
 	elif max_indx == 1:
-	  st.write("A hand with two lines (life-brain) joined")
+		st.write("A hand with two lines (life-brain) joined")
+		
 	else:
-	  st.write("A hand with all lines (life-brain-heart) seperated")
+		st.write("A hand with all lines (life-brain-heart) seperated")
 
 
 	
